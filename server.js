117a4add614df32f9783bb993c71e3ecdb397f08
Software Developer <145810690+uskhan1278@users.noreply.github.com>
2024-11-26 16:17:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
-// require('dotenv').config();
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
