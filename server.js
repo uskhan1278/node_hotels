1434b696846e6151ae6ec6ecb1234d1e11c7bb76
Software Developer <145810690+uskhan1278@users.noreply.github.com>
2024-11-26 15:53:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+// require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
+// const PORT = process.env.PORT || 3000;
 
 
 // Import the router files
