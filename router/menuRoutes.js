d1444d08ee7d2bb8640f7932125ecd1434d6fdfe
Software Developer <145810690+uskhan1278@users.noreply.github.com>
2024-11-28 13:25:31 +0530

    Update menuRoutes.js

--- router/menuRoutes.js
+++ router/menuRoutes.js
@@ -23,7 +23,7 @@ router.post('/', async (req, res) => {
   })
   
   
-  // GET Method to get Manu Item
+  //  Get Manu Item
   router.get('/',async (req, res) => {
     try{
       const data = await MenuItem.find()
@@ -34,6 +34,49 @@ router.post('/', async (req, res) => {
       res.status(500).json({error: 'Internal Sever Error'});
     }
   })
+
+
+  // Update Menu Data
+  router.put('/:id', async(req, res) =>{
+    try{
+        const menuItemId = req.params.id; // Exteact the Id from the URL parameter
+        const updateMenuData = req.body; //Updated data for the menu
+
+        const response = await MenuItem.findByIdAndUpdate(menuItemId, updateMenuData,  {
+          new: true, // Return the updated document 
+          runValidators: true, // Run Mongoose validation
+        })
+
+        if(!response){
+          return res.status(404).json({error: 'Menu Item not found'});
+        }
+
+        console.log('Data Updated');
+        res.status(200).json(response);
+    }catch(err){
+      console.log(err);
+      res.status(500).json({error: 'Internal Sever Error'});
+    }
+  })
+
+  // Delete Menu Item Data
+router.delete('/:id', async(req, res) => {
+  try{
+    const menuItemId = req.params.id; // Exteact the Id from the URL parameter
+
+    // Assuming you have a Menu model
+    const response = await MenuItem.findByIdAndDelete(menuItemId);
+    if(!response){
+      return res.status(404).json({error:'Menu Item not found' });
+    }
+    console.log('Data Delete');
+    res.status(200).json({message: 'Menu Item Deleted Successfully'});
+  }catch(err){
+    console.log(err);
+    res.status(500).json({error: 'Internal Sever Error'});
+  }
+})
+
 
 // this is testing 
   module.exports = router;
